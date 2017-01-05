0858eee9f296bd47ff013e8722a424e2b4fc5e5e
ktakayama <takayama.k@pucp.edu.pe>
2017-01-04 21:51:47 -0500

    Add Express Router for API

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
 
   var port = process.env.PORT || 3000;
 
+  var router = express.Router();
+
+  router.route('/books')
+      .get(function(request,response){
+        var responseJson = {response: "This is my API"};
+        response.json(responseJson);
+      });
+
+  app.use('/api',router);
+
   app.get('/',function(request,response){
     response.send('WELCOME TO MY API');
   });
